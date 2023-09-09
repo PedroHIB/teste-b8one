c4023ce3a82a71f4f8653ecdd25a51fe4bd01eda
Pedro Barros <pedro.barros@jussi.com.br>
2023-09-09 20:42:38 -0300

    fix: rename assets

--- src/App/Components/WishList-btn/index.jsx
+++ src/App/Components/WishList-btn/index.jsx
@@ -26,7 +26,7 @@ function WishList({ id }) {
 
     return (
         <button className='wish-list' onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} onClick={addItem}>            
-            <img src={`/Assets/wishList-${ 
+            <img src={`/assets/wishList-${ 
                 hover ? (arrayContainsValue(wishList, id) ? 'hover-active' : 'hover-default') : 
                 ( arrayContainsValue(wishList, id) ? 'active' : 'default' ) 
             }.svg`} alt="Lista de Desejos" />           
